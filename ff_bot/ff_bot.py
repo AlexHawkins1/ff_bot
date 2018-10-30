c9e45c73b08ecdb3773ad468a29e48286baf1706
AlexHawkins1 <alex.hawkins1@gmail.com>
2018-10-30 17:07:01 -0400

    fix

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -189,6 +189,7 @@ def random_phrase():
                ############# No New Facts Below This Line ##########################
                'Adam set the record for most points in a season while missing the playoffs at a staggering 1643.9 earning him the Collin J Hennessey Sodium Chloride Award.'
                ]
+               
     text = [random.choice(phrases)]
     return '\n'.join(text)
 
