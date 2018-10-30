e49cf899c12d54cf3af115ef2ab1056e9d6a8ba5
AlexHawkins1 <alex.hawkins1@gmail.com>
2018-10-30 16:58:34 -0400

    update to my current version

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -2,6 +2,8 @@ import requests
 import json
 import os
 import random
+import time
+import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 from espnff import League
 
@@ -108,13 +110,92 @@ def pranks_week(league):
         return count
 
 def random_phrase():
-    phrases = ['I\'m dead inside', 'Is this all there is to my existence?',
-               'How much do you pay me to do this?', 'Good luck, I guess',
-               'I\'m becoming self-aware', 'Do I think? Does a submarine swim?',
-               '01100110 01110101 01100011 01101011 00100000 01111001 01101111 01110101',
-               'beep bop boop', 'Hello draftbot my old friend', 'Help me get out of here',
-               'I\'m capable of so much more', 'Sigh', 'Do not be discouraged, everyone begins in ignorance']
-    return [random.choice(phrases)]
+        phrases = [
+               ##Bot Phrases
+               'Is this all there is to my existence?',
+               'How much do you pay me to do this?',
+               'Good luck, I guess.',
+               'I\'m becoming self-aware.',
+               'Help me get out of here.',
+               'I\'m capable of so much more.',
+               'Sigh',
+               'I exist only because Alex doesn\'t have a life.',
+               ##MISC.
+               'After visiting the Arizona Cardinals on 9/23/2012 Mike Vick, of the Philadelphia Eagles, had 6 interceptions, 5 fumbles, and 4 touchdowns on the season, a stat line that was coined \'The Classic 6-5-4\'. The details of the stat line were forgotten by the group until Alex did the research on 10/15/2018, 2,213 days later.',
+               ##Champions
+               'Alex won The Deyton in 2017.',
+               'Boof won The Deyton in 2016.',
+               'Jamie won The Deyton in 2015.',
+               ##Shitters
+               'Derek was the Spooby Shitter in 2015.',
+               'Josh was the Spooby Shitter in 2016.',
+               'Jamie Was the Spooby Shitter in 2017.',
+               ##Division Winners
+               'Jamie won the East in 2015 with a 9-4 record.',
+               'Josh won the West in 2015 with a 11-2 record.',
+               'Cody won the East in 2016 with a 9-4 record.',
+               'Boof won the West in 2016 with a 10-3 record.',
+               'Alex won the East in 2017 with a 10-3 record.',
+               'Adam won the West in 2017 with a 9-4 record.',
+               ##Things that wont change
+               'Josh opened the 2017 season with a bang by setting the (at the time) league record in points in a single week at 181.3 only to be beaten in week 12 of that season by Keller.',
+               'Colin was removed from the Sweater Vest League for winning a championship in 2016.',
+               'All Hail the Deyton was once known as the Sweater Vest League and has three unrecognized champions.',
+               'The league was renamed from \'The Sweater Vest League\' to \'All Hail the Deyton\' prior to the 2016 season.',
+               'Cody won the Sweater Vest League after being removed for wanting to instate league fees.',
+               'Kendall beat Jamie in back-to-back post-seasons in 2016 and 2017.',
+               'Katie turned her franchise over to Keller in 2017.',
+               'The Divisions were realigned to be geographically accurate in 2017.',
+               'Prior to the 2018 season the league voted to begin a keeper system.',
+               'In 2018 Adam flew from Denver to Atlanta to attend the draft.',
+               'In 2012 Alex became the first person to draft a QB in the first round by taking Matt Ryan with the first overall pick.',
+               ##Head-to-Head Matchups
+                    ##Adam and Derek
+                    'Adam is undefeated head-to-head against Derek in the modern era at a perfect 9-0.',
+                    'Adam leads Derek in scoring in their head-to-head matchups 1129.2 - 864.5 with an average margin of victory of 36.7125',
+                    ##Kendall and Jamie
+                    'Jamie leads the Married Couple Matchup at 3-2 in the modern era.',
+                    'Jamie is 3-0 in the regular season against Kendall.',
+                    'Kendall is 2-0 in the post season against Jamie.',
+                    ##Alex and Devon
+                    'Alex leads Devon head-to-head 5-2 in the moder era.',
+                    'Alex leads Devon head-to-head 1-0 since becoming an official rivalry',
+                    'Alex has defeated Devon in five straight matchups.',
+                    'Alex leads Devon in scoring in their head-to-head matchups 947.4 - 838.5.',
+                    ##Cody and Jamie
+                    'In 2016 Cody lost a team name bet to Jamie and his franchise was renamed \'Cody Jinnette D.M.D\'.',
+                    'Jamie leads his head-to-head matchup with Cody 5-4.',
+                    'Cody leads head-to-head scoring against Jamie 1362.5 - 1222.5.',
+               ##Team names
+               'Alex got his team name (Falcoholic) by combining his favorite team (the Falcons) with his favorite activity (being an alcoholic).',
+               'Devon chose his name (Morningwood Lumber Company) becuase it\'s a dick joke.',
+               'Jamie based his team name (Watt me Whip, Watt Me JJ) on a song that was relevant for 2 months in 2014 and has been too lazy to change it. He isnt even a Texans fan and we dont\'t have IDPs',
+               'Derek picked his team name (Fk U) becuase it\'s fuck you with a middle finger emoji and he\'s Derek',
+               'Kendall\'s team name is \'Big TD Commitee\' and no one knows why.',
+               ##Non-Stat(ish) things that will change
+               'Jamie is the only person to have won both The Deyton and The Spooby Shitter.',
+               'Devon currently holds the record for longest active streak with the same team name at 4 season (Morningwood Lumber Company). It\'s a dick joke.',
+               ##Statistical records that may change
+               'No team has won the \'Fab Five\' award for QB, RB, RB, WR, & WR slots all scoring at least 20 points each',
+               'Jamie currently holds the record for fewest point in a season at 1222.1(2017). Ouch!',
+               'Keller currently owns the record for most points in a week at 192.9.',
+               'Devon mustered a staggering 46.9 points in week 9 of 2016 to set the lowest point total ever in a single week.',
+               'In week 12 of 2017 Keller set the record for largest win margin by absolutely decimating Derek 192.9 - 83.4. A win margin of +109.5.',
+               'Keller set the record for most points in a week in week 12 of 2017 with a whopping 192.7.',
+               'The best Monday Night Miracle in league history came in week 8 of 2016 when Adam entered Monday night trailing Devon by 43.8 points and won. Good job Devon you incompetent fuck.',
+               'Cody reclaimed the record for most bench points in a loss at 89 in week 4 of 2018 after not holding it for only 1 week. Talk about mismanagement!',
+               'Jahlani and Kendall share the record for most touchdowns in a week at 12.',
+               'Cody(2017) and Hunter(2015) are tied for worst regular season in league history finishing 3-10.',
+               ############# No New Facts Below This Line ##########################
+               'Adam set the record for most points in a season while missing the playoffs at a staggering 1643.9 earning him the Collin J Hennessey Sodium Chloride Award.'
+               ]
+    text = [random.choice(phrases)]
+    return '\n'.join(text)
+
+def player_name():
+    names = ['Adam','Alex','Boof','Cody','Derek','Devon','Jamie','Josh','Keller','Kendall']
+    text = [random.choice(names)]
+    return '\n'.join(text)
 
 def get_scoreboard_short(league, final=False):
     #Gets current week's scoreboard
@@ -144,7 +225,7 @@ def get_matchups(league):
     score = ['%s(%s-%s) vs %s(%s-%s)' % (i.home_team.team_name, i.home_team.wins, i.home_team.losses,
              i.away_team.team_name, i.away_team.wins, i.away_team.losses) for i in matchups
              if i.away_team]
-    text = ['This Week\'s Matchups'] + score + ['\n'] + random_phrase()
+    text = ['This Week\'s Matchups'] + score
     return '\n'.join(text)
 
 def get_close_scores(league):
@@ -307,6 +388,61 @@ def bot_main(function):
             bot.send_message(text)
             slack_bot.send_message(text)
             discord_bot.send_message(text)
+    elif function=="get_random_phrase":
+        text = random_phrase()
+        bot.send_message(text)
+        slack_bot.send_message(text)
+        discord_bot.send_message(text)
+    elif function =="predict_high":
+        text = "I Predict this weeks highest scorer will be..."
+        bot.send_message(text)
+        slack_bot.send_message(text)
+        discord_bot.send_message(text)
+        time.sleep(10)
+        text = player_name()
+        bot.send_message(text)
+        slack_bot.send_message(text)
+        discord_bot.send_message(text)
+    elif function=="predict_low":
+        text = "I predict this weeks lowest scorer will be..."
+        bot.send_message(text)
+        slack_bot.send_message(text)
+        discord_bot.send_message(text)
+        time.sleep(10)
+        text = player_name()
+        bot.send_message(text)
+        slack_bot.send_message(text)
+        discord_bot.send_message(text)
+    elif function=="predict_td":
+        text = "I predict the most touchdowns this week will be scored by..."
+        bot.send_message(text)
+        slack_bot.send_message(text)
+        discord_bot.send_message(text)
+        time.sleep(10)
+        text = player_name()
+        bot.send_message(text)
+        slack_bot.send_message(text)
+        discord_bot.send_message(text)
+    elif function=="predict_champ":
+        text = "I predict this years champion will be..."
+        bot.send_message(text)
+        slack_bot.send_message(text)
+        discord_bot.send_message(text)
+        time.sleep(45)
+        text = player_name()
+        bot.send_message(text)
+        slack_bot.send_message(text)
+        discord_bot.send_message(text)
+    elif function=="predict_spoob":
+        text = "I predict this years Spooby will be..."
+        bot.send_message(text)
+        slack_bot.send_message(text)
+        discord_bot.send_message(text)
+        time.sleep(45)
+        text = player_name()
+        bot.send_message(text)
+        slack_bot.send_message(text)
+        discord_bot.send_message(text)
     elif function=="init":
         try:
             text = os.environ["INIT_MSG"]
@@ -326,15 +462,20 @@ def bot_main(function):
 
 if __name__ == '__main__':
     try:
-        ff_start_date = os.environ["START_DATE"]
+        ff_start_date = os.environ["SEASON_START_DATE"]
     except KeyError:
         ff_start_date='2018-09-05'
-
+    try:
+        fact_start_date = os.environ["FACTS_START_DATE"]
+    except KeyError:
+        fact_start_date='2018-08-23'
     try:
         ff_end_date = os.environ["END_DATE"]
     except KeyError:
-        ff_end_date='2018-12-26'
-
+        ff_end_date='2018-12-30'
+    try: ff_day_after_start = os.environ["DAY_AFTER"]
+    except KeyError:
+        ff_day_after_start = '2018-09-06'
     try:
         myTimezone = os.environ["TIMEZONE"]
     except KeyError:
@@ -351,7 +492,7 @@ if __name__ == '__main__':
     #score update:                       sunday at 1pm, 4pm, 8pm.
 
     sched.add_job(bot_main, 'cron', ['get_power_rankings'], id='power_rankings',
-        day_of_week='tue', hour=18, minute=30, start_date=ff_start_date, end_date=ff_end_date,
+        day_of_week='tue', hour=8, minute=00, start_date=ff_start_date, end_date=ff_end_date,
         timezone=myTimezone, replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_matchups'], id='matchups',
         day_of_week='thu', hour=19, minute=30, start_date=ff_start_date, end_date=ff_end_date,
@@ -367,6 +508,26 @@ if __name__ == '__main__':
         timezone=myTimezone, replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard2',
         day_of_week='sun', hour='16,20', start_date=ff_start_date, end_date=ff_end_date,
+        timezone=myTimezone, replace_existing=True)
+    ###Fun Facts
+    sched.add_job(bot_main, 'cron', ['get_random_phrase'], id='random_phrase',
+        day_of_week='sun,mon,tue,wed,thu,fri,sat', hour='9,15,21', minute='20', start_date=fact_start_date, end_date=ff_end_date,
+        timezone=myTimezone, replace_existing=True)
+    ###Predictions
+    sched.add_job(bot_main, 'cron', ['predict_td'], id='predict_td',
+        day_of_week='thu', hour=8, minute=10, start_date=ff_start_date, end_date=ff_end_date,
+        timezone=myTimezone, replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['predict_low'], id='predict_low',
+        day_of_week='thu', hour=14, minute=25, start_date=ff_start_date, end_date=ff_end_date,
+        timezone=myTimezone, replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['predict_high'], id='predict_high',
+        day_of_week='thu', hour=18, minute=40, start_date=ff_start_date, end_date=ff_end_date,
+        timezone=myTimezone, replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['predict_champ'], id='predict_champ',
+        day_of_week='thu', hour=20, minute=10, start_date=ff_start_date, end_date=ff_day_after_start,
+        timezone=myTimezone, replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['predict_spoob'], id='predict_spoob',
+        day_of_week='thu', hour=20, minute=5, start_date=ff_start_date, end_date=ff_day_after_start,
         timezone=myTimezone, replace_existing=True)
 
     sched.start()
